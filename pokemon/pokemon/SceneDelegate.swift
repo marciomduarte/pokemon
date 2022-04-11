7faa5f5f9c629f1c1c2d31c4a567b8bbf9feaef9
Márcio Duarte <marcio.duarte@itsector.pt>
2022-04-11 19:31:31 +0100

    +add configs and SplashScreen

--- pokemon/pokemon/SceneDelegate.swift
+++ pokemon/pokemon/SceneDelegate.swift
@@ -16,7 +16,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        guard let windowScene = (scene as? UIWindowScene) else {
+            return
+        }
+
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.makeKeyAndVisible()
+        window?.rootViewController = self.setRootViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,6 +55,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    func setRootViewController() -> UIViewController {
+        //TabBarController
+
+        let vc = ViewController()
+        return vc
+    }
 
 }
 
