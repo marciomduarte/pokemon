3c7c238524191b6aea22d89ad81f96739def587a
Márcio Duarte <marcio.duarte@itsector.pt>
2022-04-17 13:55:15 +0100

    !fix: Set loading to center of the screen

--- pokemon/pokemon/Utilities/PokemonUtils.swift
+++ pokemon/pokemon/Utilities/PokemonUtils.swift
@@ -117,8 +117,8 @@ extension UIViewController {
             self.view.addSubview(UIViewController.activityView!)
 
             UIViewController.activityViewIndicator = UIActivityIndicatorView(style: .large)
-            let minX: CGFloat = (UIScreen.main.bounds.width / 2) - ((UIViewController.activityViewIndicator?.frame.width ?? 0.0) / 2)
-            let minY: CGFloat = (UIScreen.main.bounds.height / 2) - ((UIViewController.activityViewIndicator?.frame.height ?? 0.0) / 2)
+            let minX: CGFloat = (UIScreen.main.bounds.width / 2) - ((UIViewController.activityViewIndicator?.frame.width ?? 0.0) / 3)
+            let minY: CGFloat = (UIScreen.main.bounds.height / 2) - ((UIViewController.activityViewIndicator?.frame.height ?? 0.0) / 3)
             UIViewController.activityViewIndicator?.center = CGPoint(x: minX, y: minY)
             UIViewController.activityViewIndicator?.color = UIColor.pokemonRedColor
             self.view.addSubview(UIViewController.activityViewIndicator!)
